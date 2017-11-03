492c60991ecda786d72eff2bd5ed2988eef35c88
nasiahur <Nasiahur@gmail.com>
2017-11-03 19:52:24 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,136 @@
+<footer class="footer_wrap footer_custom footer_custom_313 footer_custom_footer scheme_dark bg-footer-style">
+	<div class="vc_row wpb_row vc_row-fluid">
+		<div class="wpb_column vc_column_container vc_col-sm-12 sc_layouts_column_icons_position_left"><div class="vc_column-inner ">
+			<div class="wpb_wrapper">
+				<div class="vc_empty_space"   style="height: 6em" ><span class="vc_empty_space_inner"></span>
+				</div>
+				<div class="vc_empty_space  hide_on_mobile"   style="height: 2.2em" ><span class="vc_empty_space_inner"></span>
+				</div>
+				<div id="sc_content_1167892091" class="sc_content color_style_default sc_content_default sc_float_center sc_content_width_1_1">
+					<div class="sc_content_container">
+					<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-4 sc_layouts_column_icons_position_left">
+						<div class="vc_column-inner "><div class="wpb_wrapper">
+							<div  class="wpb_single_image wpb_content_element vc_align_center">
+		
+								<figure class="wpb_wrapper vc_figure">
+									<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="75" height="75" src="wp-content/uploads/2017/06/ic8.png" class="vc_single_image-img attachment-full" alt="" /></div>
+								</figure>
+							</div>
+							<div class="vc_empty_space"   style="height: 1.7em" ><span class="vc_empty_space_inner"></span></div>
+							<div class="vc_empty_space  hide_on_mobile"   style="height: 1em" ><span class="vc_empty_space_inner"></span></div>
+							<div class="sc_layouts_item"><div id="sc_title_2055737649"
+									class="sc_title color_style_default sc_title_default go-center"><h5 class="sc_item_title sc_title_title sc_align_center sc_item_title_style_default sc_item_title_tag">Tentang kami</h5></div><!-- /.sc_title -->
+							</div>
+							<div class="vc_empty_space"   style="height: 1.5em" ><span class="vc_empty_space_inner"></span>
+							</div>
+
+							<div class="wpb_text_column wpb_content_element  vc_custom_1498728942693" >
+								<div class="wpb_wrapper">
+									<p style="text-align: center;">Sebagai project mata kuliah Proyek Perangkat Lunak, <br>By A-TEAM</p>
+
+								</div>
+							</div>
+							<div class="vc_empty_space"   style="height: 2em" ><span class="vc_empty_space_inner"></span></div>
+							</div>
+						</div>
+						</div>
+						<div class="wpb_column vc_column_container vc_col-sm-4 sc_layouts_column_icons_position_left">
+							<div class="vc_column-inner ">
+							<div class="wpb_wrapper">
+								<div  class="wpb_single_image wpb_content_element vc_align_center">
+		
+									<figure class="wpb_wrapper vc_figure">
+										<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="75" height="75" src="wp-content/uploads/2017/06/ic9.png" class="vc_single_image-img attachment-full" alt="" /></div>
+									</figure>
+								</div>
+								<div class="vc_empty_space"   style="height: 1.7em" ><span class="vc_empty_space_inner"></span></div>
+								<div class="vc_empty_space  hide_on_mobile"   style="height: 1em" ><span class="vc_empty_space_inner"></span></div>
+								<div class="sc_layouts_item"><div id="sc_title_1286605789"
+										class="sc_title color_style_default sc_title_default go-center"><h5 class="sc_item_title sc_title_title sc_align_center sc_item_title_style_default sc_item_title_tag">Waktu kerja</h5></div><!-- /.sc_title --></div><div class="vc_empty_space"   style="height: 1.5em" ><span class="vc_empty_space_inner"></span>
+								</div>
+
+								<div class="wpb_text_column wpb_content_element " >
+									<div class="wpb_wrapper">
+										<p style="text-align: center;">Mon-Fri: 9 AM - 6 PM<br />
+																		Saturday: 9 AM - 4 PM<br />
+																		<a >Emergency Available!</a></p>
+
+									</div>
+								</div>
+								<div class="vc_empty_space"   style="height: 2em" ><span class="vc_empty_space_inner"></span></div>
+							</div>
+							</div>
+						</div>
+						<div class="wpb_column vc_column_container vc_col-sm-4 sc_layouts_column_icons_position_left">
+							<div class="vc_column-inner ">
+								<div class="wpb_wrapper">
+									<div  class="wpb_single_image wpb_content_element vc_align_center">
+		
+										<figure class="wpb_wrapper vc_figure">
+											<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="75" height="75" src="wp-content/uploads/2017/06/ic10.png" class="vc_single_image-img attachment-full" alt="" /></div>
+										</figure>
+									</div>
+									<div class="vc_empty_space"   style="height: 1.7em" ><span class="vc_empty_space_inner"></span></div>
+									<div class="vc_empty_space  hide_on_mobile"   style="height: 1em" ><span class="vc_empty_space_inner"></span></div>
+									<div class="sc_layouts_item"><div id="sc_title_1500296281"
+											class="sc_title color_style_default sc_title_default go-center"><h5 class="sc_item_title sc_title_title sc_align_center sc_item_title_style_default sc_item_title_tag">Kontak kami</h5></div><!-- /.sc_title --></div><div class="vc_empty_space"   style="height: 1.5em" ><span class="vc_empty_space_inner"></span>
+									</div>
+
+									<div class="wpb_text_column wpb_content_element " >
+										<div class="wpb_wrapper">
+											<p style="text-align: center;">Univeritas Al Azhar Indonesia<br />
+																			Phone: (021) 791 81 567<br />
+																			Blog:  <a href="www.ateamblogsite.wordpress.com">ateamblogsite.wordpress.com</a><br />
+																			Email: <a href="www.gmail.com">ateam@gmail.com</a></p>
+
+										</div>
+									</div>
+									<div class="vc_empty_space"   style="height: 2em" ><span class="vc_empty_space_inner"></span></div>
+									
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div><!-- /.sc_content -->
+			</div>
+		</div>
+	<div class="vc_row wpb_row vc_row-fluid">
+		<div class="wpb_column vc_column_container vc_col-sm-12 sc_layouts_column sc_layouts_column_align_center sc_layouts_column_icons_position_left">
+			<div class="vc_column-inner ">
+				<div class="wpb_wrapper"><div class="vc_empty_space"   style="height: 3em" ><span class="vc_empty_space_inner"></span></div>
+				<div id="sc_content_1273557644" class="sc_content color_style_default sc_content_default sc_float_center sc_content_width_1_1">
+					<div class="sc_content_container">
+						<div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_border_width_2 vc_sep_pos_align_center vc_separator_no_text vc_custom_1498734784692  vc_custom_1498734784692" >
+							<span class="vc_sep_holder vc_sep_holder_l">
+							<span  style="border-color:#39417d;" class="vc_sep_line"></span></span>
+							<span class="vc_sep_holder vc_sep_holder_r">
+							<span  style="border-color:#39417d;" class="vc_sep_line"></span></span>
+						</div>
+						<div class="vc_empty_space"   style="height: 3.6em" ><span class="vc_empty_space_inner"></span></div>
+						
+						<div class="vc_empty_space"   style="height: 2.2em" ><span class="vc_empty_space_inner"></span></div>
+
+						<div class="vc_empty_space"   style="height: 0.8em" ><span class="vc_empty_space_inner"></span></div>
+							
+							
+							<div class="vc_empty_space"   style="height: 2.5em" ><span class="vc_empty_space_inner"></span></div>
+							<div  class="vc_wp_text wpb_content_element">
+								<div class="widget widget_text">			
+									<div class="textwidget"><p><a href="" target="_blank" rel="noopener noreferrer">A TEAM</a> All rights reserved. 	<a href="" target="_blank" rel="noopener noreferrer"></a></p>
+									</div>
+								</div>
+							</div>
+							<div class="vc_empty_space"   style="height: 2.5em" ><span class="vc_empty_space_inner"></span></div>
+					</div>
+				</div><!-- /.sc_content -->
+				</div>
+			</div>
+		</div>
+	
+	
+		</div>
+	</div>
+	</div>
+</footer><!-- /.footer_wrap -->
---
+++ header.php
@@ -0,0 +1,64 @@
+<header class="top_panel top_panel_custom top_panel_custom_20 top_panel_custom_header-fullwidth without_bg_image scheme_default">
+			<div class="vc_row wpb_row vc_row-fluid">
+				<div class="wpb_column vc_column_container vc_col-sm-12 sc_layouts_column_icons_position_left">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+							<div class="vc_empty_space"   style="height: 0.5em" ><span class="vc_empty_space_inner"></span></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="vc_row wpb_row vc_row-fluid vc_custom_1498565053317 sc_layouts_row sc_layouts_row_type_normal sc_layouts_row_delimiter sc_layouts_row_fixed">
+				<div class="wpb_column vc_column_container vc_col-sm-3 sc_layouts_column sc_layouts_column_align_left sc_layouts_column_icons_position_left">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+							<div class="sc_layouts_item">
+								<a href="index.php" id="sc_layouts_logo_1422871100" class="sc_layouts_logo sc_layouts_logo_default"><img class="logo_image" src="images/logo.jpg" alt="" width="518" height="82"></a><!-- /.sc_layouts_logo -->
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="wpb_column vc_column_container vc_col-sm-9 sc_layouts_column sc_layouts_column_align_right sc_layouts_column_icons_position_left"><div class="vc_column-inner ">
+					<div class="wpb_wrapper"><div class="sc_layouts_item">
+						<nav id="sc_layouts_menu_1762481884" class="sc_layouts_menu sc_layouts_menu_default menu_hover_fade hide_on_mobile extra_margin" data-animation-in="fadeInUpSmall" data-animation-out="fadeOutDownSmall">
+						<ul id="menu-main-menu" class="sc_layouts_menu_nav">
+							<li id="menu-item-151" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-151"><a href="#"><span>Beranda</span></a>
+							</li>
+							<li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-152"><a href="#"><span>Orang Tua</span></a>
+															</li>
+							<li id="menu-item-266" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-266"><a href="#"><span>Tempat Penitipan Anak</span></a>
+								
+							</li>
+							<li id="menu-item-163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-163"><a href="#"><span>Tentang kami</span></a>
+							</li>
+							<li id="menu-item-163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-163"><a href="#"><span>Kontak kami</span></a>
+								
+							</li>
+						</ul>
+					</nav><!-- /.sc_layouts_menu -->
+					<div class="sc_layouts_iconed_text sc_layouts_menu_mobile_button">
+						<a class="sc_layouts_item_link sc_layouts_iconed_text_link" href="#">
+							<span class="sc_layouts_item_icon sc_layouts_iconed_text_icon trx_addons_icon-menu"></span>
+						</a>
+					</div>
+					</div>
+					<div class="sc_layouts_item sc_layouts_hide_on_mobile sc_layouts_hide_on_tablet">
+						<div id="sc_layouts_iconed_text_1002585016" class="sc_layouts_iconed_text hide_on_tablet hide_on_mobile">
+						
+						<span class="sc_layouts_item_details_line2 sc_layouts_iconed_text_line2"> Guest</span></span>
+						</div><!-- /.sc_layouts_iconed_text -->
+					</div>
+					<div class="sc_layouts_item sc_layouts_hide_on_mobile"><a href="order-page/index.html" id="sc_button_1801525671" class="sc_button hide_on_mobile color_style_dark sc_button_default sc_button_size_normal sc_button_icon_left"><span class="sc_button_text"><span class="sc_button_title">Login</span></span><!-- /.sc_button_text --></a><!-- /.sc_button -->
+					
+					<div id="below_login_wrapper">
+                        <div class="links">
+                            <center><a href="register.php" id="register_link">Register</a></center>
+                        </div>
+                    </div>
+					</div>
+					</div>
+					</div>
+				</div>
+			</div>
+			
+		</header>
