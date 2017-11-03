17d1ddad8e4e3555cd6cf6bd5beec542a56735b8
nasiahur <Nasiahur@gmail.com>
2017-11-03 19:51:27 +0700

    index awal belum ada detail homepage nya

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js scheme_default">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<?php include "head.php";?>
+</head>
+
+<body class="home page-template-default page page-id-299 custom-background frontpage body_tag scheme_default blog_mode_home body_style_wide  is_stream blog_style_excerpt sidebar_hide expand_content remove_margins header_style_header-custom-20 header_position_default menu_style_top no_layout wpb-js-composer js-comp-ver-5.1.1 vc_responsive">
+	<div class="body_wrap">
+		<div class="page_wrap">
+		
+		<?php include "header.php"; ?>
+			
+			
+			
+		
+			
+		<?php include "footer.php"; ?>	
+		</div><!-- /.page_wrap -->
+
+	</div><!-- /.body_wrap -->
+
+<?php include "script.php"; ?>	
+<a href="#" class="trx_addons_scroll_to_top trx_addons_icon-up" title="Scroll to top"></a>
+</body>
+
+</html>
